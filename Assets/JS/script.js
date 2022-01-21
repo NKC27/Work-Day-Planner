2c16664f3e8572b48684496d2ca1801f1ef905d7
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-01-21 18:22:06 +0000

    format of time adjusted

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,15 @@
-// Moment.js needed to display curent date Under Title @top of the page! format for 
-
 // A $( document ).ready() block.
 // $( document ).ready(function() {
 //     console.log( "ready!" );
 // });
 
+// Moment.js needed to display curent date Under Title @top of the page! format for 
 $(document).ready(function () {
 
 var displayDate = document.getElementById('currentDay');
-var nowDate = moment().format('LLLL');
+var nowDate = moment().format('MMMM Do YYYY H:mm:ss');
 displayDate.innerHTML = nowDate;
-
+var currentHour = moment().format("HH");
 })
-console.log('Is this working??')
+
+
