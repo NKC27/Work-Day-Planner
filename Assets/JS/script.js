b655d75b2aa109a03f93e83f9c26c9cfe44829b2
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-01-21 18:36:58 +0000

    clear page & local storage, id added for btn clear

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -12,4 +12,15 @@ displayDate.innerHTML = nowDate;
 var currentHour = moment().format("HH");
 })
 
+// Button function to clear local storage and clear contents of user input in display
+$('#clear').click(function (event) {
+    event.preventDefault;
+    // The .val() method is primarily used to get the values of form elements such as input, select and textarea.
+    $('textarea').val('');
+    // clear() method in JavaScript is used for the removal of all the elements from a map and make it empty. It removes all the [key, value] from the map
+    localStorage.clear();
+})
+
+
+ 
 
