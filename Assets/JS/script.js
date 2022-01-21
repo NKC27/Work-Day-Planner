5cbdcce5ae8fccd213306b277d19eb44f29b61c4
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-01-21 17:45:56 +0000

    moment bringing in date & linking js file to html

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+// Moment.js needed to display curent date Under Title @top of the page! format for 
+
+// A $( document ).ready() block.
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+
+$(document).ready(function () {
+
+var displayDate = document.getElementById('currentDay');
+var nowDate = moment().fomat('LLLL');
+
+})
+
